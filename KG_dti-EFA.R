68ef9af5d16a3e75750c475d84a78fb616f57b82
kel-github <getkellygarner@gmail.com>
2020-05-22 10:04:45 +1000

    no hello world

--- KG_dti-EFA.R
+++ KG_dti-EFA.R
@@ -99,7 +99,3 @@ plot(orth.fit.1$values, type="lines") # look at the eigenvalues
 abline(h=1, lty=2) # data suggests a 3 factor solution
 orth.fit.3.fact <- principal(mhl.mat, factors=3, rotation="varimax")
 print.psych(orth.fit.3.fact, cut=0.3, sort=TRUE)
-
-# 6. Print hello world
-# ----------------------
-print('hello world')
