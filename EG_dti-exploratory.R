20d06d3bce4da5d769d872ae258fbee530bdc02e
elizabethgeary <65267035+elizabethgeary@users.noreply.github.com>
2020-06-08 14:03:37 +1000

    Part 2

--- EG_dti-exploratory.R
+++ EG_dti-exploratory.R
@@ -4,29 +4,60 @@
 # remaining data as a csv file.
 
 rm(list=ls())
-
-# set working directory to current location
-# install.packages("rstudioapi")  # uncomment and run if you don't already have this package installed
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working directory to the location of this file
+install.packages("rstudioapi")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # load packages and source data-wrangling functions
 # --------------------------------------------------------------------------------
-# install.packages("tidyverse")  # uncomment and run if you don't have tidyverse 
-# installed already
 library(tidyverse)
 source("KG_data-wrangling.R")
 source("R_rainclouds.R")
 
 # load data
 # --------------------------------------------------------------------------------
-fpath <- '~/Dropbox/QBI/pathways-to-practice/dti-data/' # filepath to where the data lives
-tracts <- list(c("Precentral_gyrus_Left", "Middle_Frontal_gyrus_Left"), # amend this to contain which tracts you seek
-               c("Supplementary_motor_area_Left", "Rolandic_Operculum_Left"))  # has to be written exactly as is written in the list file
+fpath <- '~/Documents/THESIS/pathways-to-practice/dti-data/'
+tracts <- list(c("Superior_Frontal_gyrus_dorsolateral_Left", "Anterior_cingulate_and_paracingulate_gyri_Left"), # amend this to contain which tracts you seek
+               c("Superior_Frontal_gyrus_dorsolateral_Right", "Anterior_cingulate_and_paracingulate_gyri_Right"),
+               c("Superior_Frontal_gyrus_dorsolateral_Left", "Anterior_cingulate_and_paracingulate_gyri_Right"),
+               c("Superior_Frontal_gyrus_dorsolateral_Right", "Anterior_cingulate_and_paracingulate_gyri_Left"),
+               c("Superior_Frontal_gyrus_dorsolateral_Left", "Caudate_nucleus_Left"),
+               c("Superior_Frontal_gyrus_dorsolateral_Right", "Caudate_nucleus_Right"),
+               c("Superior_Frontal_gyrus_dorsolateral_Left", "Caudate_nucleus_Right"),
+               c("Superior_Frontal_gyrus_dorsolateral_Right", "Caudate_nucleus_Left"),
+               c("Superior_Frontal_gyrus_dorsolateral_Left", "Lenticular_nucleus_putamen_Left"),
+               c("Superior_Frontal_gyrus_dorsolateral_Right", "Lenticular_nucleus_putamen_Right"),
+               c("Superior_Frontal_gyrus_dorsolateral_Left", "Lenticular_nucleus_putamen_Right"),
+               c("Superior_Frontal_gyrus_dorsolateral_Right", "Lenticular_nucleus_putamen_Left"))
+
 sub.data <- GetDTIData(fpath, tracts)
+
+sub.data$group <- as.factor(sub.data$group)
+levels(sub.data$group) <- c("practice", "control")
+
+sub.data$session <- as.factor(sub.data$session)
+levels(sub.data$session) <- c("pre-trial", "post-trial")
+
+sub.data$tract_names <- c("lDLPFC_lACC", "rDLPFC_rACC", "lDLPFC_rACC", "rDLPFC_lACC",
+                          "lDLPFC_lCN", "rDLPFC_rCN", "lDLPFC_rCN", "rDLPFC_lCN",
+                          "lDLPFC_lLNP", "rDLPFC_rLNP", "lDLPFC_rLNP", "rDLPFC_lLNP")
+
+
+sub.data %>%
+  arrange(desc(FA))
+
+attach(sub.data)
+mytable <- table(group,session) # A will be rows, B will be columns 
+mytable # print table 
+
+margin.table(mytable, 1) # A frequencies (summed over B) 
+margin.table(mytable, 2) # B frequencies (summed over A)
 
 # 1. tidy dataframe up
 # --------------------------------------------------------------------------------
-# chapter 15 of https://r4ds.had.co.nz/factors.html will help you hear
+# chapter 15 of https://r4ds.had.co.nz/factors.html will help you here
+
+
+
 
 
 # 2. basic data check
