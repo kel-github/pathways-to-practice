09bda241962ae9a6495778ea8205f4622bd6b072
kel-github <getkellygarner@gmail.com>
2020-08-12 16:23:48 +1000

    data prep w measure accounting for skew

--- KG_prepCV-NBST.R
+++ KG_prepCV-NBST.R
@@ -6,11 +6,17 @@ rm(list=ls())
 # variables
 library(tidyverse)
 library(wesanderson)
+library(cowplot)
+library(brainGraph)
+source("R_rainclouds.R")
 source("KG_data-wrangling.R")
 dat.save.loc = 'cleaned-data'
-save.names = c('CV-regressors-multi-first','CV-regressors-multi-second')
-save.cols = matrix( c(6, 3, 4, 6, 3, 5), byrow=F, nrow=3 )
-excl.subs <- c("103", "204", "225", "226") # we don't have DTI data for these subs
+save.names = c('SkCV-regressors-s', 'SkCV-regressors-FM', 'SkCV-regressors-SM')
+save.cols = matrix( c(6, 3, 6, 4, 6, 5), byrow=F, nrow=2 )
+excl.subs <- c("103", "133", "204", "225", "226", "232") # we don't have DTI data for these subs (or pre-data, see below)
+# save coordinates of the atlas used
+#co.ords = aal116 %>% select(c("x.mni", "y.mni", "z.mni"))
+#write_csv(co.ords, paste("dti-data", "/", "aal116-coords", ".txt", sep=""), col_names=FALSE)
 ## --------------------------------------------------------------------------
 # load behavioural data and relabel
 source("KG_behaviour-wrangles.R")
@@ -18,18 +24,21 @@ levels(dat.recoded$sess) <- c("Pre", "Post")
 
 ## --------------------------------------------------------------------------
 # Computations
-# 1st, compute the mean and var by condition
+# using https://stats.stackexchange.com/questions/243498/coefficient-of-variation-with-population-median
 head(dat.recoded)
 CV.dat <- dat.recoded %>% group_by(sub, sess, mult_cond) %>%
-  summarise(RTmu = mean(RT, na.rm=T),
-            RTsd = sd(RT, na.rm=T)) %>%
+  summarise(q25 = quantile(RT, 0.25),
+            q50 = quantile(RT, 0.5),
+            q75 = quantile(RT, 0.75)) %>%
   group_by(sub, sess, mult_cond) %>%
-  transmute(CV=RTsd/RTmu) %>%
+  transmute(CV=((q75-q25)/q50)*100) %>%
   filter(sess == 'Pre') 
+
 
 ## --------------------------------------------------------------------------
 # Put into wideform and check Mahalanobis distance
 # for subs 133 and 232 we only have pre single trial data (this could be due to a recoding issue)
+
 CV.Mal.Chk <- CV.dat %>% 
   pivot_wider(names_from = mult_cond, values_from = CV) %>%
   drop_na()
@@ -44,7 +53,20 @@ sprintf("For a Mahalanobis to be less that .1 per cent likely to have occured by
 # no exclusions :)
 
 ## --------------------------------------------------------------------------
+# plot values to check distributions
+CV.p <- ggplot(CV.dat, aes(x=mult_cond, y=CV, fill = mult_cond, colour = mult_cond)) +
+  geom_flat_violin(position = position_nudge(x = .25, y = 0), adjust =2, trim =
+                     TRUE) +
+  geom_point(position=position_jitter(width=.15), size=.25) +
+  geom_boxplot(aes(x = mult_cond, y = CV), outlier.shape = NA,
+               alpha = 0.3, width = .1, colour = "BLACK") +
+  scale_y_continuous(limits=c(0,100)) + coord_flip() +
+  ylab('condition') + xlab('CV') + theme_cowplot() + 
+  guides(fill = FALSE, colour = FALSE) +
+  theme(axis.title.x = element_text(face = "italic")) 
 
+
+# ---------------------------------------------------------------------------
 # form pre-data for NBS toolbox
 '%notin%' <- Negate('%in%')
 CV.4.save <- CV.dat %>% 
